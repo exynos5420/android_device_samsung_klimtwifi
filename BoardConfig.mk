185fcca910c64edd4eedf2ba38fe83d938cdd90f
Paul Keith <javelinanddart@gmail.com>
2016-10-22 13:07:16 -0700

    Correct CMHW Include
    Change-Id: I0d7c3487b3a2461c4c9ab694a55227a093da7829
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,7 @@ TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Hardware
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
-BOARD_HARDWARE_CLASS += device/samsung/klimtlte/cmhw
+BOARD_HARDWARE_CLASS += device/samsung/klimtwifi/cmhw
 
 # Keymaster
 BOARD_USES_TRUST_KEYMASTER := true
