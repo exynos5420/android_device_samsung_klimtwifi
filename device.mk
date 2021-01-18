5344d27cab38a3af74c9213e47998befec4fb844
retiredtab-gerrit <clrh1tablet@gmail.com>
2021-01-18 01:01:08 -0700

    klimtwifi: Update dalvik heap
    Applications like youtube vanced won't even play a video and gives the error message in adb logcat
    
     01-17 23:51:57.743  I  [1295/1319] android.youtub Starting a blocking GC Alloc
     01-17 23:51:57.743  I  [1295/1326] android.youtub Waiting for a blocking GC Alloc
    
    and eventually runs out of memory and crashes.
    
    Values were taken from chagallwifi device.mk file at
    
    https://github.com/exynos5420/android_device_samsung_chagallwifi/blob/lineage-16.0/device.mk
    
    Change-Id: I3b926633229b882f85e1397f098e8df80a46a3a1

--- device.mk
+++ device.mk
@@ -33,6 +33,15 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1600
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=288m \
+    dalvik.vm.heapsize=768m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     fingerprintd \
